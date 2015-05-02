d51779ba8e686021143e17fdd348f20bb9f60ac7
Erikas Neverdauskas <erikasnever@hotmail.com>
2015-05-02 14:59:00 +0300

    Render all tasks in body

--- src/Todo.App.Web/Scripts/TodoApp/app.js
+++ src/Todo.App.Web/Scripts/TodoApp/app.js
@@ -7,36 +7,54 @@
     };
 
     TodoApp.Models.Task = Backbone.Model.extend({
-        urlRoot : "api/v1/tasks/",
+        urlRoot : 'api/v1/tasks/',
 
         defaults: {
             isCompleted: false
         }
     });
 
+    TodoApp.Collections.Tasks = Backbone.Collection.extend({
+        model: TodoApp.Models.Task,
+
+        url: 'api/v1/tasks/'
+    });
+
+    TodoApp.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+
+        render: function () {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+
+        addOne: function(task) {
+            var taskView = new TodoApp.Views.Task({ model: task });
+            this.$el.append(taskView.render().el);
+        }
+    });
+
+    TodoApp.Views.Task = Backbone.View.extend({
+        tagName: 'li',
+
+        render: function () {
+            this.$el.html(this.model.get('title'));
+            return this;
+        }
+    });
+
+    var tasksCollection = new TodoApp.Collections.Tasks;
+    tasksCollection.fetch().done(function () {
+        var tasksView = new TodoApp.Views.Tasks({ collection: tasksCollection });
+        $(document.body).append(tasksView.render().el);
+    });
+
 })();
 
 /*var Router = Backbone.Router.extend({
     initialize: function() {
-        /*if (window['initialData'] != undefined) {
-            this.syncBannerModel = new SyncBannerModel(initialData);
-            ;
-            this.syncBannerView = new SyncBannerView({
-                model: this.syncBannerModel
-            });
-            this.syncBannerView.render();
-        } else {
-            $.modal(
-                {
-                    title: multiPanelResources.errorTitle,
-                    body: multiPanelResources.initialData,
-                    buttons: [{ title: multiPanelResources.ok}]
-                
-                });
-        }#1#
+        
     }
 });
-
 var router = new Router;
-
 Backbone.history.start();*/
